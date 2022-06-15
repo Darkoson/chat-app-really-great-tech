734dc145ae193b14563f9d72893a4a8dfc8175c1
Thomas Darko <darkothomas80@gmail.com>
2022-06-15 04:01:08 +0000

    creation of hook to handle the responsiveness

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
+import useWindow from "./hooks/useWindow";
 import Chat from "./pages/chat/chat";
 import Welcome from "./pages/welcome/welcome";
 
 function App() {
+  const { width } = useWindow();
+  if (width <= 768) {
+    // do something
+    console.log("mobile mode, from App component !");
+  } else {
+    console.log("desktop mode, from App component !");
+  }
+
   return (
     <div>
       <BrowserRouter>
